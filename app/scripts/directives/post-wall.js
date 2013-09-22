df3681ddaeb134b8483e83bc7b9f4fb3fd8abe48
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-09-22 09:32:45 +0800

    Add post-it configuration form

--- app/scripts/directives/post-wall.js
+++ app/scripts/directives/post-wall.js
@@ -1,7 +1,36 @@
 'use strict';
 
 angular.module('postItApp')
-  .directive('postWall', function () {
+  .directive('postItForm', function () {
+    function controller($scope) {
+      $scope.newPostIt = {};
+      $scope.postItList = [
+        {src: 'http://china.nba.com/media/teamLogos/medium/ATL.png'},
+        {src: 'http://china.nba.com/media/teamLogos/medium/BKN.png'}
+      ];
+      $scope.addPostIt = function () {
+        var newPostIt = {
+          src: $scope.newPostIt.src.trim()
+        };
+        if (!newPostIt.src.length) {
+          return;
+        }
+        $scope.postItList.push(newPostIt);
+        $scope.newPostIt = {};
+      };
+      $scope.removePostIt = function (index) {
+        $scope.postItList.splice(index, 1);
+      };
+    }
+    return {
+      templateUrl: 'views/post-it-form-tpl.html',
+      restrict: 'E',
+      controller: ['$scope', controller],
+      link: function postLink(scope, elemen, attrs) {
+      }
+    }
+  })
+  .directive('postItWall', function () {
     return {
       template: '<div></div>',
       restrict: 'E',
